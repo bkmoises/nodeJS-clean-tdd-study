fe4420bc24aa54f9d3fb1452a7f9df8b2d19b7ac
bkmoises <bk_moises@hotmail.com>
2023-03-15 21:14:17 -0300

    fix: fix coverage test that no shows tests in to terminal

--- jest.config.ts
+++ jest.config.ts
@@ -1,12 +1,12 @@
 module.exports = {
-  collectCoverage: true,
-  collectCoverageFrom: ['<rootDir/src/**/*.ts'],
-  coverageDirectory: 'coverage',
-  coverageProvider: 'v8',
-  testEnvironment: 'node',
   roots: [
     '<rootDir>/src'
   ],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  testEnvironment: 'node',
   transform: {
     '.+\\.ts$': 'ts-jest'
   }
